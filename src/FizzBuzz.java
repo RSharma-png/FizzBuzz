e76298b0a97c96ea356df6918a15256897682d4a
Rhea Sharma <rhea.sharma.0481@gmail.com>
2024-09-09 11:53:42 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,7 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        /*for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +29,29 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+        } */
+        int i = 0;
+        while (i < 100) {
+            i = doFizzBuzz(i);
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        // Print result
+        if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("FizzBuzz");
+        }
+        else if (i % 3 == 0) {
+            System.out.println("Fizz");
+        }
+        else if (i % 5 == 0) {
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+        // Increase i  by one
+        i++;
+        return i;
     }
 }
